0f4d92633bca80bb9dda974957bea6404b8931c1
mdswaley <mdswaley075@gmail.com>
2025-01-02 20:46:47 +0530

    Setter injection using xml (for objects)

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Alien {
     private int age;
+    private Laptop lap;
 
     public Alien() {
         System.out.println("Alien Obj created");
@@ -16,7 +17,16 @@ public class Alien {
         this.age = age;
     }
 
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
+        lap.compile();
         System.out.println("coding...");
     }
 
--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -5,4 +5,9 @@ public class Laptop {
     public Laptop(){
         System.out.println("laptop obj created");
     }
+
+    public void compile(){
+        System.out.println("compiling..");
+    }
+
 }
