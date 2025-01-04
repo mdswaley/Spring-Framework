a89087df9d6700f6585900ecbf82dcf509929dff
mdswaley <mdswaley075@gmail.com>
2025-01-04 19:43:49 +0530

    lazy-init in object

--- src/main/java/org/example/Desktop.java
+++ src/main/java/org/example/Desktop.java
@@ -1,6 +1,10 @@
 package org.example;
 
+
 public class Desktop implements Computer{
+    public Desktop(){
+        System.out.println("Desktop obj created...");
+    }
     @Override
     public void compile() {
         System.out.println("Compile using Desktop..");
--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Laptop implements Computer {
+    public Laptop(){
+        System.out.println("Laptop obj created...");
+    }
     @Override
     public void compile(){
         System.out.println("compile using Laptop..");
