ae401de619cb758013fdb7287624a0706b29f8db
mdswaley <mdswaley075@gmail.com>
2025-01-13 18:51:58 +0530

    adding more features

--- src/main/java/org/example/Repository/StudentRepo.java
+++ src/main/java/org/example/Repository/StudentRepo.java
@@ -5,10 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
